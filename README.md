7a9edcb023471b4b3df4d29a8990d1a67ed19c9c
ajwl <ajwleach@gmail.com>
2017-11-08 16:07:12 +0000

    Add party and restructure

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # ig-catalonia-polltracker
+
 Polltracker and other graphics for the Catalonia 2017 regional election
+
+## Data
+The data from this app comes from a Bertha spreadsheet that is manually updated. Ask Aleks W. for access.
+
+## To regenerate with new data
+1) Add the new data to the above spreadsheet (changing column names may cause problems, see below)
+2) In the #graphics-dev channel on Slack, command build bot to rebuild this app: `buildbot build ig-catalonia-polltracker`
+3) The s3 url where the latest svg image is hosted should be overwritten with the
+new image within a few minutes. The new image should also be visible here:
+
+## Problems?
+Changing column names - especially party names may cause problems.
+
+## What this app does
+This app uses the pattern
--- index.js
+++ index.js
@@ -15,7 +15,7 @@ const {makePolltracker} = require('./lib/polltracker/index.js');
 // Catalan data comes from Bertha sheet
 const spreadsheetKey = '1k0Om8fwwSnxOolpkGGOjBHlffEeMvQwtVM6gQs_AfYE';
 const partyNames = [ "erc", "cs", "psc", "pdecat", "catcomu", "pp", "cup"]; // these parties must all be findable in the dataset & have at least some results data, if they are null, the app breaks
-const dir = 'dist';
+const timestamp = d3.timeFormat('%d-%m-%Y')(new Date());
 
 // CHART CONFIG
 const medChartConfig = {
@@ -23,8 +23,8 @@ const medChartConfig = {
 	width: 700,
 	height: 650,
 	chartPadding: {bottom: 0, top: 0},
-	chartMargin: {bottom: -90, top: 145, left: 30, right: 50},
-	sourceSizing: {sourcePos: 20},
+	chartMargin: {bottom: -70, top: 145, left: 30, right: 50},
+	sourceSizing: {sourcePos: 40},
 	title: 'Catalonia polltracker',
   radius: 5,
 };
@@ -34,7 +34,7 @@ const smallChartConfig = {
 	height: 600,
 	chartPadding: {bottom: 0, top: 0},
 	chartMargin: {bottom: -110, top: 170, left: 20, right: 28},
-	sourceSizing: {sourcePos: 25},
+	sourceSizing: {sourcePos: 45},
 	title: 'Catalonia polltracker',
   radius: 3,
 };
@@ -46,8 +46,8 @@ loadData().then(data =>{
   const chartS = makePolltracker(sortedData, lineData, smallChartConfig);
   writeChartToFile(chartM, 'medium');
   writeChartToFile(chartS, 'small');
-  writeHoldingPage(timestamp, dir);
-}).catch(err => console.error(`MAKING THE CHART FAILED ${err}`, err));
+  writeHoldingPage(timestamp);
+}).catch(err => console.error(`MAKING THE CHART FAILED: => `, err));
 
 
 async function loadData(){
