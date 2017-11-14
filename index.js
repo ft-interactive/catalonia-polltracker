dc2d1c7ebea25dc020ed3965e7df1a9e06854620
ajwl <ajwleach@gmail.com>
2017-11-14 17:55:35 +0000

    Add bar charts

--- index.js
+++ index.js
@@ -11,13 +11,14 @@ const {sortData} = require('./lib/sort-data.js');
 const {averageData} = require('./lib/average-data.js');
 const { writeChartToFile, writeHoldingPage } = require('./lib/write-files.js');
 const {makePolltracker} = require('./lib/polltracker/index.js');
+const {makeBarchart} = require('./lib/barchart/index.js');
 
 // Catalan data comes from Bertha sheet
 const spreadsheetKey = '1k0Om8fwwSnxOolpkGGOjBHlffEeMvQwtVM6gQs_AfYE';
 const partyNames = [ "erc", "cs", "psc", "pdecat", "catcomu", "pp", "cup"]; // these parties must all be findable in the dataset & have at least some results data, if they are null, the app breaks
 const timestamp = d3.timeFormat('%d-%m-%Y')(new Date());
 
-// CHART CONFIG
+// CHART CONFIG POLLTRACKER
 const medChartConfig = {
 	frameMaker: chartFrame.webFrameM,
 	width: 700,
@@ -39,16 +40,44 @@ const smallChartConfig = {
   radius: 3,
 };
 
+
+// CHART CONFIG BARCHART
+const medChartConfigBar = {
+	frameMaker: chartFrame.webFrameM,
+	width: 700,
+	height: 650,
+	chartPadding: {bottom: 0, top: 0},
+	chartMargin: {bottom: -70, top: 145, left: 30, right: 50},
+	sourceSizing: {sourcePos: 40},
+	title: 'Catalonia independence vote'
+};
+const smallChartConfigBar = {
+	frameMaker: chartFrame.webFrameS,
+	width: 300,
+	height: 600,
+	chartPadding: {bottom: 0, top: 0},
+	chartMargin: {bottom: -110, top: 170, left: 20, right: 28},
+	sourceSizing: {sourcePos: 45},
+	title: 'Catalonia independence vote'
+};
+
 loadData().then(data =>{
   const sortedData = sortData(data.data, partyNames);
-  const lineData = averageData(sortedData);
-  const chartM = makePolltracker(sortedData, lineData, medChartConfig);
-  const chartS = makePolltracker(sortedData, lineData, smallChartConfig);
+  const averagedData = averageData(sortedData);
+
+  const chartM = makePolltracker(sortedData, averagedData, medChartConfig);
+  const chartS = makePolltracker(sortedData, averagedData, smallChartConfig);
+
+  const barM = makeBarchart(averagedData, medChartConfigBar);
+  const barS = makeBarchart(averagedData, smallChartConfigBar);
+
   writeChartToFile(chartM, 'medium');
   writeChartToFile(chartS, 'small');
+  writeChartToFile(barM, 'bar-medium');
+  writeChartToFile(barS, 'bar-small');
+
   writeHoldingPage(timestamp);
 }).catch(err => console.error(`MAKING THE CHART FAILED: => `, err));
-
 
 async function loadData(){
   return bertha.get(spreadsheetKey, ['data'], { republish: true });
